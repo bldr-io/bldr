7bd107a9d258d28f28c5334ca57efbbb1a311325
Aaron Scherer <aequasi@gmail.com>
2014-03-24 22:53:23 -0700

    Removing test that cant be tested

--- tests/Command/BuildCommandTest.php
+++ tests/Command/BuildCommandTest.php
@@ -69,52 +69,6 @@ class BuildCommandTest extends \PHPUnit_Framework_TestCase
         $commandTester->execute(['command' => $command->getName()]);
     }
 
-    public function testExecuteCommaTasks()
-    {
-        $container = \Mockery::mock('Symfony\Component\DependencyInjection\ContainerBuilder');
-        $container->shouldReceive('findTaggedServiceIds')
-            ->andReturn(['exec']);
-        $container->shouldReceive('get')
-            ->withArgs(['exec'])
-            ->andReturn(new MockCall());
-
-        $application  = new Application();
-        Config::$NAME = '.test';
-
-        $config = [
-            'name'        => 'test',
-            'description' => 'test app',
-            'profiles'    => [
-                'default' => [
-                    'description' => 'test profile',
-                    'tasks'       => [
-                        'test'
-                    ]
-                ]
-            ],
-            'tasks'       => [
-                'test' => [
-                    'calls' => [
-                        [
-                            'type'      => 'exec',
-                            'arguments' => ['ls -l']
-                        ]
-                    ]
-                ]
-            ]
-        ];
-
-        $application->setConfig(Config::create('yml', $config));
-
-        $application->add(new BuildCommand());
-
-        $command = $application->find('build');
-        $command->setContainer($container);
-        $commandTester = new CommandTester($command);
-
-        $commandTester->execute(['command' => $command->getName()], ['tasks' => 'test,test2']);
-    }
-
     /**
      * @expectedException \Exception
      */
