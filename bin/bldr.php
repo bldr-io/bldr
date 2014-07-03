f0085aebd4e0073439f19f695a3fe32f138adb8e
Luis Cordova <cordoval@gmail.com>
2014-07-02 23:50:47 -0500

    make meta header and run it

--- bin/bldr.php
+++ bin/bldr.php
@@ -1,12 +1,12 @@
 <?php
 
 /**
- * This file is part of Bldr.io.
+ * This file is part of Bldr.io
  *
  * (c) Aaron Scherer <aequasi@gmail.com>
  *
  * This source file is subject to the MIT license that is bundled
- * with this source code in the file LICENSE.
+ * with this source code in the file LICENSE
  */
 
 set_time_limit(0);
--- src/Exception/ConfigurationFileNotFoundException.php
+++ src/Exception/ConfigurationFileNotFoundException.php
@@ -1,11 +1,11 @@
 <?php
 
 /**
- * This file is part of bldr
+ * This file is part of Bldr.io
  *
  * (c) Aaron Scherer <aequasi@gmail.com>
  *
- * This source file is subject to the license that is bundled
+ * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE
  */
 
