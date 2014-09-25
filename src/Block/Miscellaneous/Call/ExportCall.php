8e1724cf4e63ce6456201de994e5eb60b5fe1e9b
Luis Cordova <cordoval@gmail.com>
2014-09-25 01:22:27 -0500

    plug export call on miscellaneous block

---
+++ src/Block/Miscellaneous/Call/ExportCall.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * This file is part of Bldr.io
+ *
+ * (c) Aaron Scherer <aequasi@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE
+ */
+
+namespace Bldr\Block\Miscellaneous\Call;
+
+use Bldr\Call\AbstractCall;
+
+/**
+ * @author Aaron Scherer <aequasi@gmail.com>
+ */
+class SleepCall extends AbstractCall
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function configure()
+    {
+        $this->setName('sleep')
+            ->setDescription('Sleep for the given amount of time')
+            ->addOption('seconds', true, 'Milliseconds to sleep for.')
+        ;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function run()
+    {
+        sleep($this->getOption('seconds'));
+
+        return true;
+    }
+}
